b5280e16f2c4d77f60e9b1f504500d1d08797ba5
roundowl <airbus.sai@gmail.com>
2016-01-10 22:44:39 +0300

    Added plugin description, added base for a window
    Window will show only Hello World and is not interactable. Not tested.

--- PI_Autopilot.py
+++ PI_Autopilot.py
@@ -1,20 +1,41 @@
-#   Copyright 2016 Andrey Seliverstov
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+'''
+Copyright 2016 Andrey Seliverstov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+--------------------------------
+Python Autopilot plugin by Round Owl
+Version 0.7
+
+This plugin substitutes flight control surface management.
+
+Problem that this plugin tries to solve:
+When you set "override_control_surfaces" to "1", X-Plane drops all
+control over the plane, meaning that you can't fly with joystick,
+mouse or by using autopilot.
+
+This plugin will start working once both of these are true:
+1) Flight director and autopilot are turned on;
+2) Landing lights are off.
+After plugin starts working, it will constantly try to follow flight
+director commands, and counter any yawing with rudder.
+'''
 
 from XPLMUtilities import *
 from XPLMProcessing import *
 from XPLMDataAccess import *
+from XPLMDisplay import *
+from XPLMGraphics import *
+from XPLMMenu import *
 import math
 
 class PythonInterface:
@@ -22,7 +43,12 @@ class PythonInterface:
         self.Name = "Python Autopilot"
         self.Sig = "RoundOwl.Autopilot"
         self.Desc = "Basic implementation of autopilot in python. Use and modify as you wish."
-
+                
+		self.DrawWindowCB = self.DrawWindowCallback
+        self.KeyCB = self.KeyCallback
+        self.MouseClickCB = self.MouseClickCallback
+		self.WindowId = XPLMCreateWindow(self, 50, 600, 300, 400, 1, self.DrawWindowCB, self.KeyCB, self.MouseClickCB, 0)
+        
         #----------------
         # Adjust sensitivity here.
         # Cessna 172SP: 1 roll, 1 pitch
@@ -70,17 +96,45 @@ class PythonInterface:
 
     def XPluginStop(self):
         XPLMUnregisterFlightLoopCallback(self, self.flightloopCB, 0)
+        XPLMDestroyWindow(self, self.WindowId)
         pass
 
     def XPluginEnable(self):
         return 1
 
     def XPluginDisable(self):
-        XPLMUnregisterFlightLoopCallback(self, self.flightloopCB, 0)
+        #XPLMUnregisterFlightLoopCallback(self, self.flightloopCB, 0)
+        #XPLMDestroyWindow(self, self.WindowId)
         pass
 
     def XPluginReceiveMessage(self, inFromWho, inMessage, inParam):
         pass
+        
+    #-----
+    #Next section controls window in the sim
+    #-----
+        
+    def DrawWindowCallback(self, inWindowID, inRefcon):
+		lLeft = []; lTop = []; lRight = []; lBottom = []
+		XPLMGetWindowGeometry(inWindowID, lLeft, lTop, lRight, lBottom)
+		left = int(lLeft[0]); top= int(lTop[0]); right = int(lRight[0]); bottom = int(lBottom[0])
+		gResult = XPLMDrawTranslucentDarkBox(left, top, right, bottom)
+		colour = 1.0, 1.0, 1.0
+
+		Desc = "Hello World 1"
+
+		gResult = XPLMDrawString(colour, left + 5, top - 20, Desc, 0, xplmFont_Basic)
+		pass
+
+	def KeyCallback(self, inWindowID, inKey, inFlags, inVirtualKey, inRefcon, losingFocus):
+		pass
+
+	def MouseClickCallback(self, inWindowID, x, y, inMouse, inRefcon):
+		return 1
+        
+    #-----
+    #Next section controls in-flight operation
+    #-----
 
     def CalculateCtrlSrfDegInput(self, FDInputDeg, ActualInputDeg, CurrCtrlSrfDefDeg, CtrlSrfImpact, PreviousError):
         errorDeg = math.sqrt((FDInputDeg - ActualInputDeg + PreviousError) ** 2)
@@ -98,9 +152,12 @@ class PythonInterface:
             # elvTrim is set because it used to run away and break default AP
             XPLMSetDataf(self.elvTrim, 0.0)
             
-            self.RollError = round(self.RollError + (XPLMGetDataf(self.FDRollDR) - XPLMGetDataf(self.planeRollDR))/20.0, 4) if (self.RollError ** 2) < 16 else self.RollError * 0.99
-            self.PitchError = round(self.PitchError + (XPLMGetDataf(self.FDPitchDR) - XPLMGetDataf(self.planePitchDR))/20.0, 4) if (self.PitchError ** 2) < 16 else self.PitchError * 0.99
-            #self.SlipError = round(self.SlipError + (0.0 - 2*XPLMGetDataf(self.planeSlipDR) - XPLMGetDataf(self.rudd11DefDR))/20.0, 6) if (self.SlipError ** 2) < 64 else self.SlipError * 0.99
+            self.RollError = round(self.RollError + (XPLMGetDataf(self.FDRollDR)
+                - XPLMGetDataf(self.planeRollDR))/20.0, 4) if (self.RollError ** 2) < 16 else self.RollError * 0.99            
+            self.PitchError = round(self.PitchError + (XPLMGetDataf(self.FDPitchDR)
+                - XPLMGetDataf(self.planePitchDR))/20.0, 4) if (self.PitchError ** 2) < 16 else self.PitchError * 0.99            
+            #self.SlipError = round(self.SlipError + (0.0 - 2*XPLMGetDataf(self.planeSlipDR)
+            #    - XPLMGetDataf(self.rudd11DefDR))/20.0, 6) if (self.SlipError ** 2) < 64 else self.SlipError * 0.99
 
             #desiredRudInput = self.CalculateCtrlSrfDegInput(
             #0.0, XPLMGetDataf(self.planeSlipDR) - XPLMGetDataf(self.rudd11DefDR),
